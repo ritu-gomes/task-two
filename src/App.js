c3826685985f5f2e8a8e280354163fc80e993b30
Sumit kumar <sumitkumar@Sumits-MacBook-Air.local>
2021-03-11 23:52:10 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Navbar />
+      <Main />
     </div>
   );
 }
